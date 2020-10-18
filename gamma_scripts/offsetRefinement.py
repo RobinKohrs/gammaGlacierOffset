4366c496a8e56f9c80e18fd2f5314897ec716af0
konstantinschellenberg <konstantin.schellenberg@posteo.de>
2020-10-18 10:48:36 +0200

    [offsetRefinement.py] Added arg-parsing steps, QA_reading and looping once offset_fitting

--- gamma_scripts/offsetRefinement.py
+++ gamma_scripts/offsetRefinement.py
@@ -4,23 +4,39 @@
 
 import os
 import re
+import argparse
 import sys
 
-if sys.platform == "win32":
-    pass
-else:
-    kernel = str(os.system("uname -r"))
-    print(kernel)
+##################
+# parse option to work locally or on server
+##################
 
-    if kernel == "3.10.0-957.el7.x86_64":
-        try:
-            import py_gamma as pg
-            print("py_gamma successfully loaded")
-        except ImportError as err:
-            print("The module `py_gamma` needs to be installed")
-    else:
-        print("Kernel not identical")
+# parse some arguments
+parser = argparse.ArgumentParser(description="Decide whether you are executing this locally (-l) or on the server (-s) and which steps to perform")
+# get positional arguments
+parser.add_argument("-m", "--machine", dest="m",
+                    help="(input) decide if working locally (0) or on the server (1)", default="l", type=str)
+
+parser.add_argument("-s", "--step", dest="steps",
+                    help="(input) which step to perform unzip (0), slc-import (1), dem_import (2)", default=0,
+                    nargs="+", type=int)
+
+
+# get the arguments
+global args
+args = parser.parse_args()
+print(args)
+if not args.m == "s":
+    print("working locally...")
+else:
+    try:
         import py_gamma as pg
+        print("working on the server...")
+    except ImportError as err:
+        print("Working on the server...")
+        print("However the py_gamma-module can not be loaded...")
+        print("Make sure its on $PATH? or PYTHONPATH?")
+        exit(-1)
 
 #########################################
 # USER INPUT
@@ -32,16 +48,18 @@ dates = date1 + "_" + date2
 
 dir = "../data/test"
 
-
 slc1 = os.path.join(dir, "20160112.db.slc")
 slc1_par = os.path.join(dir, "20160112.db.slc.par")
 slc2 = os.path.join(dir, "20160124.db.slc")
 slc2_par = os.path.join(dir, "20160124.db.slc.par")
 
-
 off = os.path.join(dir, dates + ".off")
 
+# 1 = Intensity tracking
+# 2 = Fringe visibility tracking
 tracking_algorithm = 1
+# tracking_algorithm = 2
+
 
 #########################################
 # CREATIN OFFSETS (actually belongs to another script
@@ -49,6 +67,7 @@ tracking_algorithm = 1
 
 def delete_offsets():
     os.remove(off)
+
 
 def create_offsets(slc1, slc2):
     print("Offset file: {}\n".format(off))
@@ -64,6 +83,7 @@ def create_offsets(slc1, slc2):
     except:
         print("not successful")
 
+
 def init_offset_orbit(slc1_par, slc2_par, off):
     print("Offset file: {}\n".format(off))
 
@@ -76,36 +96,37 @@ def init_offset_orbit(slc1_par, slc2_par, off):
     except:
         print("Not successful")
 
-def reading_QA():
-    QA = os.path.join(dir, dates + ".QA")
 
-    print(QA)
+def reading_QA(QA):
+
+    # read file
     with open(QA) as f:
         lines = f.readlines()
-        for i in lines:
-            print(i)
         f.close()
 
-    # with open(QA) as f:
-    #     lines = f.readlines()
-    #     if re.search("width", lines):
-    #         dem_width = lines
-    #     if re.search("lines", lines):
-    #         dem_lines = lines
-    #     print("Width: ", dem_width, "\n",
-    #           "Lines: ", dem_lines, "\n")
-    #     f.close()
+    # strip white space and \n
+    lst = [x.strip() for x in lines]
+
+    # grep measurement statement (first match)
+    for line in lst:
+        if re.match("^final\smodel", line) is not None:
+            statement = line
+    # print(statement)
+
+    # grep range & azimuth model fit standard deviation
+    try:
+        sd_metric = re.findall("\d.\d*", statement)
+    except:
+        "QA_temp console output document not readable"
+
+    dict = {"range":sd_metric[0], "azimuth":sd_metric[1]}
+    return (dict)
 
 
 def optimise_offsets(slc1, slc2, slc1_par, slc2_par, off):
-
     reg_offsets = os.path.join(dir, dates + ".reg_offsets")
     qmf_offsets = os.path.join(dir, dates + ".qmf_offsets")
-    QA = os.path.join(dir, dates + ".QA")
-    if os.path.isfile(QA):
-        os.remove(QA)
-
-    QA_temp = os.path.join(dir, dates + ".QA_temp")
+    QA = os.path.join(dir, dates + ".QA_temp")
 
     # Metrics static ------
     # Window size <- to be optimised
@@ -120,39 +141,64 @@ def optimise_offsets(slc1, slc2, slc1_par, slc2_par, off):
     sizes = [2 ** x for x in range(1, 10)]
     samples = [x ** 2 for x in range(5, 10)]
 
-
-
-
-    oversampling = 2 # what does that actually mean?
-    theshold = 0.001 # <- to be optimised
+    oversampling = 2  # what does that actually mean?
+    theshold = 0.001  # <- to be optimised
 
     # Sentinel-1 TOPS acquisitions need to be deramped. (Is this done already in S1_mosaic_TOPS?)
     deramping = 1
 
+    for i in enumerate(sizes):
 
-    for patch in sizes:
-        for sample in samples:
-            pass
+        print("=====")
+        print("Initiating offsets with precise Sentinel-1 orbit information")
+        print("Iteration Number: {}".format(i))
+        print("=====")
+
+        # remove QA_temp file from which it needs to be read from new at every iteration
+        # if os.path.isfile(QA):
+        #     os.remove(QA)
+        #     print("Removed QA_temp file . . .")
+        #
+        # pg.offset_pwr(slc1, slc2, slc1_par, slc2_par, off, reg_offsets, qmf_offsets,
+        #               patch_rn, patch_az, "-", oversampling, samples_rn, samples_az,
+        #               theshold, "-", "-", deramping)
+
+        sys.stdout = open(QA, 'w')
+        pg.offset_fit(reg_offsets, qmf_offsets, off, "-", "-")
+        sys.stdout 
+
+        qa_read = reading_QA(QA)
+        print("Metrics:", qa_read)
+        break
+
+        # for sample in samples:
+        #     pass
 
 
-    pg.offset_pwr(slc1, slc2, slc1_par, slc2_par, off, reg_offsets, qmf_offsets,
-                  patch_rn, patch_az, "-", oversampling, samples_rn, samples_az,
-                  theshold, "-", "-", deramping)
 
-    # saving console output to file
-
-    # TODO: output is saved in file, not really functional...
-    sys.stdout = open(QA, 'w')
-    pg.offset_fit(reg_offsets, qmf_offsets, off, "-", "QA")
 
     # os.remove(QA_temp)
 
 
 def main():
+
+    print(args.steps)
+    for step in sorted(args.steps):
+        if int(step) == 0:
+            create_offsets(slc1_par, slc2_par)
+        elif int(step) == 1:
+            init_offset_orbit(slc1_par, slc2_par, off)
+        elif int(step) == 2:
+            optimise_offsets(slc1, slc2, slc1_par, slc2_par, off)
+        elif int(step) == 3:
+            reading_QA(os.path.join(dir, dates + ".QA_temp"))
+
     # create_offsets(slc1_par, slc2_par)
     # init_offset_orbit(slc1_par, slc2_par, off)
-    optimise_offsets(slc1, slc2, slc1_par, slc2_par, off)
-    reading_QA()
+    # optimise_offsets(slc1, slc2, slc1_par, slc2_par, off)
+    # pr=reading_QA(QA)
+    # print(pr)
+
 
 if __name__ == '__main__':
     main()
