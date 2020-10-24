732bf411cfae5a5904a9ce92844db0fa28741bef
konstantinschellenberg <konstantin.schellenberg@posteo.de>
2020-10-24 18:27:31 +0200

    offsetRefinement.py tidy, __init__.py

--- gamma_scripts/offsetRefinement.py
+++ gamma_scripts/offsetRefinement.py
@@ -12,6 +12,7 @@ import sys
 from io import StringIO
 import pandas as pd
 import numpy as np
+from functions import get_files
 
 ##################
 # parse option to work locally or on server
@@ -21,11 +22,17 @@ import numpy as np
 parser = argparse.ArgumentParser(description="Decide whether you are executing this locally (-l) or on the server (-s) and which steps to perform")
 # get positional arguments
 parser.add_argument("-m", "--machine", dest="m",
-                    help="(input) decide if working locally (l) or on the server (s)", default="l", type=str)
+                    help="(input) decide if working locally (l) or on the server (s)", default="s", type=str)
 
 parser.add_argument("-s", "--step", dest="steps",
-                    help="(input) which step to perform unzip (0), slc-import (1), dem_import (2)", default=0,
+                    help="(input) which step to perform unzip (0), slc-import (1), dem_import (2)", default=[0],
                     nargs="+", type=int)
+
+# 1 = Intensity tracking
+# 2 = Fringe visibility tracking
+parser.add_argument("-a", "--algorithm", dest="trackingAlgorithm",
+                    help="(Input) Intensity Tracking (1) or Fringe Visibility Tracking (2)",
+                    default=1, type=int)
 
 parser.add_argument("-i", "--iters", dest="i",
                     help="(input) is number of iterations performed as optimisation of the offset [int]",
@@ -34,8 +41,7 @@ parser.add_argument("-i", "--iters", dest="i",
 # get the arguments
 global args
 args = parser.parse_args()
-print(args)
-print(args.i)
+
 
 if not args.m == "s":
     print("working locally...")
@@ -69,11 +75,6 @@ qmf_offsets = os.path.join(dir, dates + ".qmf_offsets")
 QA = os.path.join(dir, dates + ".QA")
 off = os.path.join(dir, dates + ".off")
 
-# 1 = Intensity tracking
-# 2 = Fringe visibility tracking
-tracking_algorithm = 1
-# tracking_algorithm = 2
-
 # Sentinel-1 TOPS acquisitions need to be deramped. (Is this done already in S1_mosaic_TOPS?)
 deramping = 1
 oversampling = 2  # what does that actually mean?
@@ -82,10 +83,6 @@ oversampling = 2  # what does that actually mean?
 #########################################
 # CREATIN OFFSETS (actually belongs to another script
 #########################################
-
-def delete_offsets():
-    os.remove(off)
-
 
 def create_offsets(slc1, slc2):
     print("Offset file: {}\n".format(off))
@@ -96,7 +93,7 @@ def create_offsets(slc1, slc2):
         print("=====")
         print("Initiating offset file from SLC parameter file")
         print("=====")
-        pg.create_offset(slc1, slc2, off, tracking_algorithm)
+        pg.create_offset(slc1, slc2, off, args.trackingAlgorithm)
 
     except:
         print("not successful")
@@ -252,7 +249,6 @@ def optimise_offsets(slc1, slc2, slc1_par, slc2_par, off):
     optimisation = {0: [64, 25, 0.1, {'metrics': {'range': '0.1085', 'azimuth': '0.1246'}}], 1: [64, 25, 0.15, {'metrics': {'range': '0.0675', 'azimuth': '0.0993'}}]}
 
     optimisation = {0: [64, 25, 0.1, {'metrics': {'range': '0.1583', 'azimuth': '0.1380'}}], 1: [64, 25, 0.15, {'metrics': {'range': '0.0675', 'azimuth': '0.0995'}}], 2: [64, 25, 0.2, {'metrics': {'range': '0.0679', 'azimuth': '0.1005'}}], 3: [64, 25, 0.25, {'metrics': {'range': '0.0583', 'azimuth': '0.1042'}}], 4: [64, 25, 0.3, {'metrics': {'range': '0.0622', 'azimuth': '0.1122'}}], 5: [64, 36, 0.1, {'metrics': {'range': '0.0740', 'azimuth': '0.1039'}}], 6: [64, 36, 0.15, {'metrics': {'range': '0.0729', 'azimuth': '0.1037'}}], 7: [64, 36, 0.2, {'metrics': {'range': '0.0660', 'azimuth': '0.0929'}}], 8: [64, 36, 0.25, {'metrics': {'range': '0.0579', 'azimuth': '0.0702'}}], 9: [64, 36, 0.3, {'metrics': {'range': '0.0560', 'azimuth': '0.0721'}}], 10: [64, 49, 0.1, {'metrics': {'range': '0.0742', 'azimuth': '0.1511'}}], 11: [64, 49, 0.15, {'metrics': {'range': '0.0739', 'azimuth': '0.1422'}}], 12: [64, 49, 0.2, {'metrics': {'range': '0.0652', 'azimuth': '0.1422'}}], 13: [64, 49, 0.25, {'metrics': {'range': '0.0584', 'azimuth': '0.1443'}}], 14: [64, 49, 0.3, {'metrics': {'range': '0.0566', 'azimuth': '0.1432'}}], 15: [128, 25, 0.1, {'metrics': {'range': '0.0499', 'azimuth': '0.0710'}}], 16: [128, 25, 0.15, {'metrics': {'range': '0.0465', 'azimuth': '0.0714'}}], 17: [128, 25, 0.2, {'metrics': {'range': '0.0452', 'azimuth': '0.0729'}}], 18: [128, 25, 0.25, {'metrics': {'range': '0.0589', 'azimuth': '0.0916'}}], 19: [128, 25, 0.3, {'metrics': {'range': '0.0605', 'azimuth': '0.0925'}}], 20: [128, 36, 0.1, {'metrics': {'range': '0.0442', 'azimuth': '0.0711'}}], 21: [128, 36, 0.15, {'metrics': {'range': '0.0435', 'azimuth': '0.0707'}}], 22: [128, 36, 0.2, {'metrics': {'range': '0.0438', 'azimuth': '0.0711'}}], 23: [128, 36, 0.25, {'metrics': {'range': '0.0410', 'azimuth': '0.0669'}}], 24: [128, 36, 0.3, {'metrics': {'range': '0.0283', 'azimuth': '0.0677'}}], 25: [128, 49, 0.1, {'metrics': {'range': '0.0600', 'azimuth': '0.1045'}}], 26: [128, 49, 0.15, {'metrics': {'range': '0.0573', 'azimuth': '0.1059'}}], 27: [128, 49, 0.2, {'metrics': {'range': '0.0446', 'azimuth': '0.1031'}}], 28: [128, 49, 0.25, {'metrics': {'range': '0.0390', 'azimuth': '0.1031'}}], 29: [128, 49, 0.3, {'metrics': {'range': '0.0392', 'azimuth': '0.1097'}}], 30: [256, 25, 0.1, {'metrics': {'range': '0.0292', 'azimuth': '0.0715'}}], 31: [256, 25, 0.15, {'metrics': {'range': '0.0295', 'azimuth': '0.0677'}}], 32: [256, 25, 0.2, {'metrics': {'range': '0.0300', 'azimuth': '0.0709'}}], 33: [256, 25, 0.25, {'metrics': {'range': '0.0301', 'azimuth': '0.0698'}}], 34: [256, 25, 0.3, {'metrics': {'range': '0.0320', 'azimuth': '0.0595'}}], 35: [256, 36, 0.1, {'metrics': {'range': '0.0310', 'azimuth': '0.0571'}}], 36: [256, 36, 0.15, {'metrics': {'range': '0.0283', 'azimuth': '0.0591'}}], 37: [256, 36, 0.2, {'metrics': {'range': '0.0279', 'azimuth': '0.0541'}}], 38: [256, 36, 0.25, {'metrics': {'range': '0.0227', 'azimuth': '0.0517'}}], 39: [256, 36, 0.3, {'metrics': {'range': '0.0260', 'azimuth': '0.0457'}}], 40: [256, 49, 0.1, {'metrics': {'range': '0.0289', 'azimuth': '0.0654'}}], 41: [256, 49, 0.15, {'metrics': {'range': '0.0225', 'azimuth': '0.0599'}}], 42: [256, 49, 0.2, {'metrics': {'range': '0.0212', 'azimuth': '0.0593'}}], 43: [256, 49, 0.25, {'metrics': {'range': '0.0197', 'azimuth': '0.0564'}}], 44: [256, 49, 0.3, {'metrics': {'range': '0.0228', 'azimuth': '0.0597'}}], 45: [512, 25, 0.1, {'metrics': {'range': '0.0160', 'azimuth': '0.0325'}}], 46: [512, 25, 0.15, {'metrics': {'range': '0.0162', 'azimuth': '0.0324'}}], 47: [512, 25, 0.2, {'metrics': {'range': '0.0148', 'azimuth': '0.0327'}}], 48: [512, 25, 0.25, {'metrics': {'range': '0.0146', 'azimuth': '0.0315'}}], 49: [512, 25, 0.3, {'metrics': {'range': '0.0154', 'azimuth': '0.0337'}}], 50: [512, 36, 0.1, {'metrics': {'range': '0.0177', 'azimuth': '0.0301'}}], 51: [512, 36, 0.15, {'metrics': {'range': '0.0173', 'azimuth': '0.0297'}}], 52: [512, 36, 0.2, {'metrics': {'range': '0.0165', 'azimuth': '0.0274'}}], 53: [512, 36, 0.25, {'metrics': {'range': '0.0154', 'azimuth': '0.0270'}}], 54: [512, 36, 0.3, {'metrics': {'range': '0.0156', 'azimuth': '0.0289'}}], 55: [512, 49, 0.1, {'metrics': {'range': '0.0176', 'azimuth': '0.0314'}}], 56: [512, 49, 0.15, {'metrics': {'range': '0.0143', 'azimuth': '0.0267'}}], 57: [512, 49, 0.2, {'metrics': {'range': '0.0135', 'azimuth': '0.0249'}}], 58: [512, 49, 0.25, {'metrics': {'range': '0.0125', 'azimuth': '0.0251'}}], 59: [512, 49, 0.3, {'metrics': {'range': '0.0097', 'azimuth': '0.0204'}}], 60: [1024, 25, 0.1, {'metrics': {'range': '0.0130', 'azimuth': '0.0152'}}], 61: [1024, 25, 0.15, {'metrics': {'range': '0.0130', 'azimuth': '0.0154'}}], 62: [1024, 25, 0.2, {'metrics': {'range': '0.0133', 'azimuth': '0.0165'}}], 63: [1024, 25, 0.25, {'metrics': {'range': '0.0131', 'azimuth': '0.0163'}}], 64: [1024, 25, 0.3, {'metrics': {'range': '0.0175', 'azimuth': '0.0226'}}], 65: [1024, 36, 0.1, {'metrics': {'range': '0.0132', 'azimuth': '0.0190'}}], 66: [1024, 36, 0.15, {'metrics': {'range': '0.0129', 'azimuth': '0.0189'}}]}
-(2, 0.2)
 
     optimisation = looping(maxiter=args.i)
 
@@ -305,6 +301,8 @@ def main():
             init_offset_orbit(slc1_par, slc2_par, off)
         elif int(step) == 2:
             optimise_offsets(slc1, slc2, slc1_par, slc2_par, off)
+        else:
+            pass
 
 if __name__ == '__main__':
     main()
