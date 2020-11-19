8bc0b1b71a752e8022bbcfdc163fb45dee6a1549
Robin Kohrs <robin.kohrs@gmx.de>
2020-11-19 19:52:14 +0100

    rewrite make dirs 00

--- gamma_scripts/00_makeFolderStructure.py
+++ gamma_scripts/00_makeFolderStructure.py
@@ -8,6 +8,7 @@ import os
 import re
 import argparse
 from functions import *
+from datetime import date
 
 ##################
 # parse option to work locally or on server
@@ -45,74 +46,91 @@ def make_folder_struc(data_dir = "../data",
     zips = [x for x in os.listdir(data_dir) if x.endswith(".zip")]
     # get all the dates
     dates = []
-    for date in zips:
+    for d in zips:
         # match the first date in the zip files
-        m = re.match(".*__1SDV_(\d{4})(\d{2})(\d{2})", date)
-        d = m.groups()
-        d_str = ''.join(d)
-        dates.append(d_str)
-
-    # make a list for summer and one for winter ??
-    summer = []
-    winter = []
-    for d in dates:
-        if int(d[4:6]) in [12,1,2]:
-            winter.append(d)
-        else:
-            summer.append(d)
-
-    # for every date in the summer
-    date_combs_summer = []
-    for d in summer:
-        # get the year
-        year = int(d[0:4])
-        for o in summer:
-            # for every other date check if the year is the same
-            if o != d:
-                year_o = int(o[0:4])
-                if year_o == year:
-                    mas = min(int(d), int(o))
-                    sl = max(int(d), int(o))
-                    dir = str(mas) + "_" + str(sl)
-
-                    # will produce the double amount
-                    if not dir in date_combs_summer:
-                        date_combs_summer.append(dir)
-
-    # print("Summer Combinations")
-    sorted_date_combs_summer = sorted(date_combs_summer)
-    # print(sorted_date_combs_summer)
+        m = re.match(".*__1SDV_(\d{4})(\d{2})(\d{2})", d)
+        da = m.groups()
+        d_str = ''.join(da)
+        dt = date(year=int(d_str[0:4]), month=int(d_str[4:6]), day=int(d_str[6:8]))
+        dates.append(dt)
 
 
-    # for every date in the winter
-    date_combs_winter = []
-    for d in winter:
-        # get the year
-        year = int(d[0:4])
-        for o in winter:
-            # for every other date check if the year is the same
-            if o != d:
-                year_o = int(o[0:4])
-                if year_o == year:
-                    mas = min(int(d), int(o))
-                    sl = max(int(d), int(o))
-                    dir = str(mas) + "_" + str(sl)
+    date_combs = []
+    for i in dates:
+        for j in dates:
+            if i != j:
+                if abs((i-j).days) < 13:
+                    if i < j:
 
-                    # will produce the double amount
-                    if not dir in date_combs_winter:
-                        date_combs_winter.append(dir)
+                        main = "".join(str(i).split("-"))
+                        second = "".join(str(j).split("-"))
+                        tuple = main + "_" + second
+                        date_combs.append(tuple)
 
+    # this modifies the actual element date_combs
+    date_combs.sort()
 
-    # print("Winter Combinations")
-    sorted_date_combs_winter = sorted(date_combs_winter)
-    # print(sorted_date_combs_winter)
-
-    # add them both to one list
-    comb_dates_all = sorted_date_combs_summer + sorted_date_combs_winter
-    print(comb_dates_all)
-    print()
-    print("There are {} pairs".format(len(comb_dates_all)))
-    print()
+    # # make a list for summer and one for winter ??
+    # summer = []
+    # winter = []
+    # for d in dates:
+    #     if int(d[4:6]) in [12,1,2]:
+    #         winter.append(d)
+    #     else:
+    #         summer.append(d)
+    #
+    # # for every date in the summer
+    # date_combs_summer = []
+    # for d in summer:
+    #     # get the year
+    #     year = int(d[0:4])
+    #     for o in summer:
+    #         # for every other date check if the year is the same
+    #         if o != d:
+    #             year_o = int(o[0:4])
+    #             if year_o == year:
+    #                 mas = min(int(d), int(o))
+    #                 sl = max(int(d), int(o))
+    #                 dir = str(mas) + "_" + str(sl)
+    #
+    #                 # will produce the double amount
+    #                 if not dir in date_combs_summer:
+    #                     date_combs_summer.append(dir)
+    #
+    # # print("Summer Combinations")
+    # sorted_date_combs_summer = sorted(date_combs_summer)
+    # # print(sorted_date_combs_summer)
+    #
+    #
+    # # for every date in the winter
+    # date_combs_winter = []
+    # for d in winter:
+    #     # get the year
+    #     year = int(d[0:4])
+    #     for o in winter:
+    #         # for every other date check if the year is the same
+    #         if o != d:
+    #             year_o = int(o[0:4])
+    #             if year_o == year:
+    #                 mas = min(int(d), int(o))
+    #                 sl = max(int(d), int(o))
+    #                 dir = str(mas) + "_" + str(sl)
+    #
+    #                 # will produce the double amount
+    #                 if not dir in date_combs_winter:
+    #                     date_combs_winter.append(dir)
+    #
+    #
+    # # print("Winter Combinations")
+    # sorted_date_combs_winter = sorted(date_combs_winter)
+    # # print(sorted_date_combs_winter)
+    #
+    # # add them both to one list
+    # comb_dates_all = sorted_date_combs_summer + sorted_date_combs_winter
+    # print(comb_dates_all)
+    # print()
+    # print("There are {} pairs".format(len(comb_dates_all)))
+    # print()
 
 
     modes = ["intensity", "fringe"]
@@ -121,7 +139,7 @@ def make_folder_struc(data_dir = "../data",
     tuple_dir = dir_tuples; os.makedirs(tuple_dir) if not os.path.isdir(tuple_dir) else print(".....")
 
     # create both if not existent
-    for d in comb_dates_all:
+    for d in date_combs:
         for m in modes:
             dirs = os.path.join(tuple_dir,d, m)
             if args.print:
