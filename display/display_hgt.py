0c9adfcb71622e55b9925feb22cd84bb60f4e1fc
Robin Kohrs <robin.kohrs@gmx.de>
2021-02-06 16:39:23 +0100

    clean backup

--- display/display_hgt.py
+++
@@ -1,8 +0,0 @@
-# displaying rasters in gamma
-
-import py_gamma as pg
-
-hgt = "../data/DEM/20200911_vv_iw2.hgt.ras"
-mli = "../data/tuples/20200911_20200923/20200911_vv_iw2.rmli"
-
-pg.disras(hgt, mli, 2481)
